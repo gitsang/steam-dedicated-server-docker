8148a61c2dc09aa528d91726c694306ea14cf6aa
sang <sang.chen@outlook.com>
2024-03-21 11:30:20 +0000

    feat: update volumes config

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 APP_ID=$APP_ID
 APP_INSTALL_DIR=$APP_INSTALL_DIR
 
-/home/steam/steamcmd/steamcmd.sh +force_install_dir $APP_INSTALL_DIR +login anonymous +app_update $APP_ID +quit
+steamcmd +force_install_dir $APP_INSTALL_DIR +login anonymous +app_update $APP_ID +quit
 
 exec "$@"
