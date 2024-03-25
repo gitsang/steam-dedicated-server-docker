ee26569b676d71d8dd50ec03692aa77d428e77a3
sang <sang.chen@outlook.com>
2024-03-25 14:10:21 +0800

    feat: entrypoint add update option

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,18 @@ set -x
 
 APP_ID=$APP_ID
 APP_INSTALL_DIR=$APP_INSTALL_DIR
+UPDATE=${UPDATE:-always}
 
-steamcmd +force_install_dir $APP_INSTALL_DIR +login anonymous +app_update $APP_ID +quit
+if [[ ${UPDATE} == "always" ]]; then
+    NEED_UPDATE=true
+elif [[ ${UPDATE} == "if_not_exist" ]]; then
+    if [[ ! -d ${APP_INSTALL_DIR} ]]; then
+        NEED_UPDATE=true
+    fi
+fi
+
+if [[ ${NEED_UPDATE} == "true" ]]; then
+    steamcmd +force_install_dir ${APP_INSTALL_DIR} +login anonymous +app_update ${APP_ID} +quit
+fi
 
 exec "$@"
